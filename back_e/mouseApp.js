bf0e6b60e38aca0d09406df8bc3ef9ae4586c0aa
Noah Brinton <noahbrinton@Noahs-MacBook-Pro.local>
2022-11-07 13:22:03 -0500

    starting backend mod

--- back_e/mouseApp.js
+++ back_e/mouseApp.js
@@ -1,36 +1,33 @@
-const express = require('express');
 const WebSocket = require('ws');
-const http = require('https');
-var fs = require('fs');
-const app = express();
-
-//initialize a simple http server
 
 const wss = new WebSocket.Server({ port:56112, clientTracking: true});
-// const wss = new WebSocket.Server({ port:8080, clientTracking: true})
-// const server = http.createServer({
-//   cert: fs.readFileSync('/etc/letsencrypt/live/brintonliteraryagency.org/cert.pem'),
-//   key: fs.readFileSync('/etc/letsencrypt/live/brintonliteraryagency.org/privkey.pem')
-// });
-// server.listen(443);
-// console.log(server);
-// const wss = new WebSocket.Server({ server, autoAcceptConnections: true});
-let connectedClients = [];
+let connectedClients = {};
+
 wss.on('request', function(request) {
         var connection = request.accept('echo-protocol', request.origin);
         console.log((new Date()) + ' Connection accepted.');
 });
 wss.on('connection', function connection(ws) {
-        console.log('connected');
-        console.log('New Connection', ws);
-        connectedClients.push(ws);
-        ws.on('message', function incoming(message) {   
+        ws.on('message', function incoming(message) { 
+
                 let m = JSON.parse(message);
                 let data = m.data;
-                console.log('received: %s');
+
+                if (!connectedClients[data.id]) {
+                        connectedClients[data.id] = {
+                                id: data.id,
+                                connected: true,
+                        };
+                }
+
+                if (data.action = "selectTeam") {
+                        connectedClients[data.id].team = teamSelection;
+                }
+
                 wss.clients.forEach(client => {
                         client.send(JSON.stringify(data))
                 });
+
         });
 
         ws.on('error', function(e) {
@@ -40,6 +37,7 @@ wss.on('connection', function connection(ws) {
         ws.on('close', function(e) {
                 console.log('close', e);
         });
-        ws.send('something');
+        
+        ws.send(JSON.stringify(connectedClients));
 });
 //start our server
